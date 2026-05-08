94485dbf6b3af639aff060de403ac1b6be3ca4f0
dishant1123 <dishantshah1417@gmail.com>
2026-05-08 19:25:09 +0530

    ....

---
+++ loop/8may.c
@@ -0,0 +1,133 @@
+/*
+pattern  : 
+
+11.        12.          13 .          14.
+* * * * *   * * * * *          *       * 
+  * * * *    * * * *         * *      * * 
+    * * *     * * *        * * *     * * * 
+      * *      * *       * * * *    * * * *
+        *       *      * * * * *   * * * * * 
+
+*/
+
+// ex :11 
+/*
+#include<stdio.h>
+int main()
+{
+    int i,j,k; 
+
+    for(i=1; i<=5; i++)  // 3   3<=5
+    {
+        for(k=1; k<i; k++) // 1   1<3 
+        {
+            printf("  ");
+        }
+        for(j=5; j>=i; j--) // 5   5 >=3
+        {
+            printf("* "); // * * * * * 
+        }                 //   * * * * 
+        printf("\n");    //      * * *
+    }
+    return 0;
+}
+*/
+// ex :12
+/*
+#include<stdio.h>
+int main()
+{
+    int i,j,k; 
+    for(i=1; i<=5; i++)  // 3   3<=5
+    {
+        for(k=1; k<i; k++) // 1   1<3 
+        {
+            printf(" ");
+        }
+        for(j=5; j>=i; j--) // 5   5 >=3
+        {
+            printf("* "); // * * * * * 
+        }                 //  * * * * 
+        printf("\n");    //    * * *
+    }
+    return 0;
+}
+*/
+
+// ex :13 
+/*
+#include<stdio.h>
+int main()
+{
+    int i,j,k; 
+
+    for(i=1; i<=5; i++)  // 3   3<=5
+    {
+        for(k=5; k>i; k--)
+        {
+            printf("  ");
+        }
+        for(j=1; j<=i; j++) // 5   5 >=3
+        {
+            printf("* "); // * * * * * 
+        }                 //   * * * * 
+        printf("\n");    //      * * *
+    }
+    return 0;
+}
+*/ 
+/*
+// ex :14 
+#include<stdio.h>
+int main()
+{
+    int i,j,k; 
+
+    for(i=1; i<=5; i++)  // 3   3<=5
+    {
+        for(k=5; k>i; k--)
+        {
+            printf(" ");
+        }
+        for(j=1; j<=i; j++) // 5   5 >=3
+        {
+            printf("* "); // * * * * * 
+        }                 //   * * * * 
+        printf("\n");    //      * * *
+    }
+    return 0;
+}
+*/ 
+// full peramid  : 
+
+#include<stdio.h>
+int main()
+{
+    int i,j,k; 
+
+    for(i=1; i<=5; i++)  // 3   3<=5
+    {
+        for(k=5; k>i; k--)
+        {
+            printf(" ");
+        }
+        for(j=1; j<=i; j++) // 5   5 >=3
+        {
+            printf("* "); // * * * * * 
+        }                 //   * * * * 
+        printf("\n");    //      * * *
+    }
+    for(i=1; i<=5; i++)  // 3   3<=5
+    {
+        for(k=1; k<i; k++) // 1   1<3 
+        {
+            printf(" ");
+        }
+        for(j=5; j>=i; j--) // 5   5 >=3
+        {
+            printf("* "); // * * * * * 
+        }                 //  * * * * 
+        printf("\n");    //    * * *
+    }
+    return 0;
+}
