b23d8bae95f0bba7c9d5af7b77694b6ec53e2744
dishant1123 <dishantshah1417@gmail.com>
2026-05-09 15:19:46 +0530

    ....

--- backup/array_backup_array.c
+++ backup/array_backup_array.c
@@ -63,12 +63,12 @@ int main()
 
 // insert : 
 /*
-int a[5] = {1,2,3,4,5}; 
+int a[5] = {1,2,3,4,5};  //4
 pos =3 ; 
 ins =90; 
 after insert  array : {1,2,3,90,4,5}
 */
-
+/*
 #include<stdio.h>
 int main()
 {
@@ -106,3 +106,89 @@ int main()
     }
     return 0; 
 }
+*/ 
+
+// bubble sort : 
+
+/*
+a[5] = {1,6,2,5,3};  
+*/
+#include<stdio.h>
+int  main()
+{
+    int a[40],num,i; 
+    printf("enter the number of elements\n");
+    scanf("%d",&num);  // 5 
+    
+    for(i=0; i<num; i++)
+    {
+        scanf("%d",&a[i]); 
+    }
+    printf("\narray is \n");
+    for(i=0; i<num; i++)
+    {                         //       0   1  2  3   4  
+        printf("%d\t",a[i]);  // a[5] ={1, 4, 3, 2,  5}
+    }
+
+    for(i=0; i<num-1; i++) // i=1  1 <4  
+    {
+        for(int j=i+1; j<num; j++) // j=2  2 <5 
+        {
+            if(a[i] > a[j]) // a[1] > a[2]     4 > 3
+            {
+                int temp = a[i];  // temp = a[1] temp =4
+                a[i] = a[j];      // a[1] = a[2] a[1] = 3
+                a[j] = temp;      // a[2] = 4    a[2] =4
+            }
+        }                          // 1 3 4 2 ,5 
+    }
+    printf("\nafter bubble sort  array element is  : \n");
+    for(i=0; i<num; i++)
+    {
+        printf("%d\t",a[i]);
+    }
+    return 0; 
+}
+
+
+
+
+/*
+for : 
+
+for(intial ; con ; inc/dec)
+*/
+
+// ex :1 1 -100
+/*
+#include<stdio.h>
+int  main()
+{
+    int i,num ;
+    printf("enter the number : \n");
+    scanf("%d",&num); 
+
+    for(i=1; i<=num; i++)
+    {
+        printf("%d\t",i);
+    }
+    return 0; 
+}
+*/ 
+//  ex :2 
+/*
+#include<stdio.h>
+int  main()
+{
+    int i,num ,sum=0;
+    printf("enter the number : \n");
+    scanf("%d",&num);  // 5 
+
+    for(i=1; i<=num; i++) // 6  6 <=5  
+    {
+        sum =sum +i;  // sum =15
+    }
+    printf("sum is : %d",sum);  // 15
+    return 0; 
+}
+*/
